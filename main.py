1147093fb8c3d14343d3520cc29c18eca15843c8
Jaewan Kim <jk5149@nyu.edu>
2017-07-26 23:15:29 +0900

    final
    added the function toInteger as well

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ def toBinary(number):
     return str(number%2)
 
 
+def toInteger(binary):
+    twos = int(1)
+
+    integer = int(0)
+    while binary > 0:
+        if binary % 10 > 0:
+            integer += twos
+        binary = int(binary/10)
+        twos *= 2
+    return integer
+
+
 def XORMultiplication(number1, number2):
     number = int(number1) * int(number2)
     output = ''
@@ -21,4 +33,8 @@ def XORMultiplication(number1, number2):
 input1 = int(input('enter the first number: '))
 input2 = int(input('enter the second number: '))
 
+print('Result in binary:')
 print(XORMultiplication(toBinary(input1),toBinary(input2)))
+
+print('Result in integer:')
+print(toInteger(int(XORMultiplication(toBinary(input1),toBinary(input2)))))
